891fb8d084154b77ee32a48a5332f2cc50bbbb7b
Spry <spr-yoshida@users.noreply.github.com>
2015-06-05 17:55:56 +0900

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML PUBLIC"-//W3C//DTD HTML4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Index</title>
+</head>
+
+<body>
+	<?php
+		require "User.php";
+		
+		print 'test</br>';
+		
+		$object = new User;
+		
+		$object->name = "Joe";
+		$object->password = "abc@123%&";
+		print_r($object);
+		
+		print '</br>';
+		
+		$object->save_user();
+	?>
+
+</body>
+</html>
